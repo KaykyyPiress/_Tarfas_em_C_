438c9dd318be28e8a10d7a2ed9f9e959e3d1d886
Kayky Pires <125900277+KaykyyPiress@users.noreply.github.com>
2023-11-09 20:29:08 +0000

    arrumando a main colocando a função 6

--- main.c
+++ main.c
@@ -13,10 +13,13 @@ int main() {
         printf("3. Listar todas as tarefas\n");
         printf("4. Alterar tarefas\n");
         printf("5. Filtros\n");
+        printf("6. Listar por prioridade\n");
+        printf("7. Listar por categoria\n");
+        printf("8. Listar por prioridade e categoria\n");
         printf("9. Sair\n");
         scanf("%d", &opcao);
 
-        switch(opcao) {
+        switch (opcao) {
             case 1:
                 adicionar_tarefas(tarefas, &numero_tarefas); // Adiciona uma nova tarefa
                 break;
@@ -29,7 +32,66 @@ int main() {
             case 4:
                 alterar_tarefas(tarefas, numero_tarefas); // Altera uma tarefa existente
                 break;
-            default:
+            case 5: {
+                int filtro;
+                printf("\nSelecione um filtro:\n");
+                printf("1. Filtrar tarefas por prioridade\n");
+                printf("2. Filtrar tarefas por estado\n");
+                printf("3. Filtrar tarefas por categoria\n");
+                printf("4. Filtrar tarefas por prioridade e categoria\n");
+                scanf("%d", &filtro);
+
+                switch (filtro) {
+                    case 1: {
+                        int prioridade;
+                        printf("Digite a prioridade desejada: ");
+                        scanf("%d", &prioridade);
+                        filtrar_tarefas_por_prioridade(tarefas, numero_tarefas, prioridade);
+                        listar_tarefas(tarefas, numero_tarefas, 20);
+                        break;
+                    }
+                    case 2: {
+                        char estado[20];
+                        printf("Digite o estado desejado: ");
+                        scanf(" %[^\n]s", estado);
+                        // Filtrar tarefas por estado
+                        // Código específico para filtrar tarefas por estado
+                        break;
+                    }
+                    case 3: {
+                        char categoria[100];
+                        printf("Digite a categoria desejada: ");
+                        scanf(" %[^\n]s", categoria);
+                        // Filtrar tarefas por categoria
+                        // Código específico para filtrar tarefas por categoria
+                        break;
+                    }
+                    case 4: {
+                        int prioridade;
+                        printf("Digite a prioridade desejada: ");
+                        scanf("%d", &prioridade);
+                        char categoria[100];
+                        printf("Digite a categoria desejada: ");
+                        scanf(" %[^\n]s", categoria);
+                        // Filtrar tarefas por prioridade e categoria
+                        // Código específico para filtrar tarefas por prioridade e categoria
+                        break;
+                    }
+                    default:
+                        printf("Opcao invalida.\n");
+                }
+                break;
+              
+            case 6:
+              {
+                  int prioridade;
+                  printf("Digite a prioridade para exportar as tarefas: ");
+                  scanf("%d", &prioridade);
+                  exportar_tarefas_por_prioridade(tarefas, numero_tarefas, prioridade);
+                  printf("Tarefas exportadas com sucesso.\n");
+                  break;
+              }
+               default:
                 printf("Opcao invalida.\n"); // Exibe uma mensagem de erro caso digite uma opção errada
         }
     } while(opcao != 9);
